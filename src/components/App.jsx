a86c41b97e6f120af1e9301fbbb93713dc5f00ab
Anna Kreidina <kav99mail@gmail.com>
2023-05-15 15:33:32 +0300

    useMemo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -20,24 +20,22 @@ export const App = () => {
       contacts.some(
         contact => contact.name.toLowerCase() === name.toLowerCase()
       )
-    ) {
+    )
       return alert(`Name: ${name} to already in contacts`);
-    }
+
     if (
       contacts.some(
         contact => contact.number.toLowerCase() === number.toLowerCase()
       )
-    ) {
+    )
       return alert(`Number: ${number} to already in contacts`);
-    }
 
     addNewContact(name, number);
   };
 
   const addNewContact = (name, number) => {
-    const idData = nanoid();
     const newContact = {
-      id: idData,
+      id: nanoid(),
       name,
       number,
     };
@@ -48,27 +46,26 @@ export const App = () => {
     setFilter(e.target.value);
   };
 
-  const visibleContact = () => {
+  const visibleContact = useMemo(() => {
     const normalizeFilter = filter.toLowerCase();
     const visibleContact = contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizeFilter)
     );
     return visibleContact;
-  };
+  }, [contacts, filter]);
 
   const deleteContact = id => {
     const newContacts = contacts.filter(contact => contact.id !== id);
     setContacts(newContacts);
   };
 
-  const visible = visibleContact();
   return (
     <div className="generalContainet">
       <h1>Phonebook</h1>
       <ContactForm onSubmit={addContact} />
       <h2>Contacts</h2>
       <Filter value={filter} onChange={filterChange} />
-      <ContactList contactNames={visible} deleteItem={deleteContact} />
+      <ContactList contactNames={visibleContact} deleteItem={deleteContact} />
     </div>
   );
 };
